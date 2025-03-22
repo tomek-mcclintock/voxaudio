6d78d636a672a62deb4b7136ca43eb48ca48e87b
tomek-mcclintock <tomekmcc@gmail.com>
2025-03-22 18:31:17 +0000

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,16 +30,7 @@ export default function Header({ companyData }: HeaderProps) {
               </div>
             )}
           </div>
-          <div className="flex items-center space-x-4">
-            <a 
-              href="https://ruggable.co.uk/help" 
-              target="_blank" 
-              rel="noopener noreferrer"
-              className="text-sm text-gray-500 hover:text-gray-700"
-            >
-              Help Center
-            </a>
-          </div>
+          {/* Removed the Help Center link div that was here */}
         </div>
       </div>
     </header>
